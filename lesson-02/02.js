9daff6010bb4fe363598780f5d21fda9d73dd5d1
ChiteS <balbesik-07@mail.ru>
2025-07-01 00:25:13 +0300

    second commit

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,9 +7,18 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
+let dice1 = Math.floor(Math.random() * 6) + 1
+let dice2 = Math.floor(Math.random() * 6) + 1
 let isWinningDouble // your code
+
+if (dice1 != dice2) {
+    isWinningDouble = false
+} else if (dice1 <= 3) {
+    isWinningDouble = false
+} else
+    isWinningDouble = true
+
+
 
 /*
 console.log('Первый бросок: ' + dice1)
