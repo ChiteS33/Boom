2efa5f1c90a4b8753e4d420ec7a4de9da309e86e
ChiteS <balbesik-07@mail.ru>
2025-07-01 15:05:40 +0300

    third commit

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,14 +1,7 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-const number_1 = 10
-const number_2 = 20
-
-
-
 function calculateAverage(a, b) {
 
     average = (a + b) / 2;
     return average
    }
-
-const executeScript= calculateAverage(number_1, number_2)
